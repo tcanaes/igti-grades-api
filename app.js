9b0726267e60a74cfc42a432f929fb28e3497488
thiago.canaes <noreply.thiago.canaes@email.com>
2020-11-10 22:35:14 -0300

    changing cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://igti-mod4-grades-frontend.herokuapp.com',
   })
 );
 
